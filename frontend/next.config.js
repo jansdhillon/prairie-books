6cbc741c7748bc5fc62913d06abb53a3f9da36cc
jansdhillon <imightbejan@gmail.com>
2024-09-02 23:32:27 -0600

    fixes to env vars

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
+    env: {
+        ADMIN_USERNAME: process.env.ADMIN_USERNAME,
+        ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
+    },
 }
 
 module.exports = nextConfig
