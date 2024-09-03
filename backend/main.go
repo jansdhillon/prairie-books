312b37e9b2bb66297a3dd8760a515c8fe2c2020a
jansdhillon <imightbejan@gmail.com>
2024-09-03 00:51:33 -0600

    rm pokcetbase add docker compose nginx

--- backend/main.go
+++ backend/main.go
@@ -1,34 +1,14 @@
-package main
+package backend
 
 import (
-	"github.com/labstack/echo/v5"
-	"github.com/pocketbase/pocketbase"
-	"github.com/pocketbase/pocketbase/apis"
-	"github.com/pocketbase/pocketbase/core"
-	"log"
+	"fmt"
 	"net/http"
 )
 
 func main() {
-	app := pocketbase.New()
-
-	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-		// add new "GET /hello" route to the app router (echo)
-		e.Router.AddRoute(echo.Route{
-			Method: http.MethodGet,
-			Path:   "/hello",
-			Handler: func(c echo.Context) error {
-				return c.String(200, "Hello world!")
-			},
-			Middlewares: []echo.MiddlewareFunc{
-				apis.ActivityLogger(app),
-			},
-		})
-
-		return nil
+	http.HandleFunc("/api/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
 	})
 
-	if err := app.Start(); err != nil {
-		log.Fatal(err)
-	}
+	http.ListenAndServe(":8080", nil)
 }
