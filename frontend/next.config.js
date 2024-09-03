f8e01b6a769f5e3bab398d81366a50b8c6eaafaa
jansdhillon <imightbejan@gmail.com>
2024-09-02 23:49:55 -0600

    undo changes to secrets

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
-    env: {
-        ADMIN_USERNAME: process.env.ADMIN_USERNAME,
-        ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
-    },
 }
 
 module.exports = nextConfig
