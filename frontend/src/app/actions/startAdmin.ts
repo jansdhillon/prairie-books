e8a2cd35a687b43d916540fc1c3c328f37fa8730
jansdhillon <imightbejan@gmail.com>
2024-09-05 22:33:11 -0600

    nevermind let's just use ts

--- frontend/src/app/actions/startAdmin.ts
+++
@@ -1,9 +0,0 @@
-"use server"
-
-import PocketBase from 'pocketbase';
-
-export const startAdmin = async () => {
-    // const pb = new PocketBase('http://127.0.0.1:8090');
-    // await pb.admins.authWithPassword(process.env.ADMIN_USERNAME, process.env.ADMIN_PASSWORD);
-    // return pb;
-}
--- frontend/src/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-export const Footer = () => {
-  return (
-    <footer className="flex flex-col gap-2 sm:flex-row justify-center pb-6 pt-4 w-full shrink-0 items-center px-4 md:px-6 border-t">
-      <p className="text-xs text-primary font-bold">
-        2024  Prairie Books &copy;
-      </p>
-    </footer>
-  );
-};
