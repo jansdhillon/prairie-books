73834ff2d96b12b5a2a10a2fb96a37c107ee2034
jansdhillon <imightbejan@gmail.com>
2024-09-12 04:23:03 -0600

    debug logs

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,15 @@ import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 
 export default async function Index() {
+  const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const anonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+  if (!url){
+    console.error("NEXT_PUBLIC_SUPABASE_URL is not defined");
+  } else if (!anonKey){
+    console.error("NEXT_PUBLIC_SUPABASE_ANON_KEY is not defined");
+  } else {
+    console.log("Supabase URL and Anon Key are defined");
+  }
   return (
     <>
       <Hero />
