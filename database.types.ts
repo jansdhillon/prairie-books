42fcc543f5fd5733235b768f1a4a217758dd7e23
jansdhillon <imightbejan@gmail.com>
2024-09-12 19:57:59 -0600

    accidentally committed corrupted file

--- database.types.ts
+++
@@ -1,8 +0,0 @@
-[?25l
-    Select a project:                                                                              
-                                                                                                   
-  >  1. molwzkmeoeezeppqbibw [name: prairie-books, org: tatoqgeznkwhzpyqgxwi, region: ca-central-1]
-                                                                                                   
-                                                                                                   
-    ↑/k up • ↓/j down • / filter • q quit • ? more                                                 
-                                                                                                   [0D[2K[?25h[?1002l[?1003l[?1006l
