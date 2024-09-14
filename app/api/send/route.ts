27da6f512f8d94575aea44e393aac37e7261a2ab
jansdhillon <imightbejan@gmail.com>
2024-09-13 19:31:32 -0600

    fix key name

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -2,7 +2,7 @@ import { EmailTemplate } from '@/components/email-template';
 import { Resend } from 'resend';
 
 
-const resend = new Resend(process.env.RESEND_API_KEY || '');
+const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
 
 
 
