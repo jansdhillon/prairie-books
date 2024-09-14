0785c8a544331b82d20d8c439aa819ce25c2658f
jansdhillon <imightbejan@gmail.com>
2024-09-13 19:03:52 -0600

    rm secrets from df and add handling for resend key

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,7 +1,10 @@
 import { EmailTemplate } from '@/components/email-template';
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+
+const resend = new Resend(process.env.RESEND_API_KEY || '');
+
+
 
 export async function POST() {
   try {
