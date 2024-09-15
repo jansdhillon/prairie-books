14cd84b10cdba2b2fe8c4f1bd314b91fa589c62c
jansdhillon <imightbejan@gmail.com>
2024-09-14 20:20:12 -0600

    rm resend for now

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,26 +1,25 @@
 import { EmailTemplate } from '@/components/email-template';
-import { Resend } from 'resend';
-
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+// import { Resend } from 'resend';
 
 
 
 export async function POST() {
-  try {
-    const { data, error } = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
-      to: ['delivered@resend.dev'],
-      subject: 'Hello world',
-      react: EmailTemplate({ firstName: 'John' }),
-    });
+  // console.log("Resend", process.env.RESEND_API_KEY)
+  // const resend = new Resend(process.env.RESEND_API_KEY);
+  // try {
+  //   const { data, error } = await resend.emails.send({
+  //     from: 'Acme <onboarding@resend.dev>',
+  //     to: ['delivered@resend.dev'],
+  //     subject: 'Hello world',
+  //     react: EmailTemplate({ firstName: 'John' }),
+  //   });
 
-    if (error) {
-      return Response.json({ error }, { status: 500 });
-    }
+  //   if (error) {
+  //     return Response.json({ error }, { status: 500 });
+  //   }
 
-    return Response.json(data);
-  } catch (error) {
-    return Response.json({ error }, { status: 500 });
-  }
+  //   return Response.json(data);
+  // } catch (error) {
+  //   return Response.json({ error }, { status: 500 });
+  // }
 }
